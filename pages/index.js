cb6b86eb35a2270351583262533502f6200d8b28
Kenny <kenny@audico.co.za>
2025-05-03 15:10:35 +0200

    fix: embed Botpress using inline iframe method for Vercel

--- pages/index.js
+++ pages/index.js
@@ -3,28 +3,12 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [quoteItems, setQuoteItems] = useState([]);
 
+  // Poll quote API every 5 seconds
   useEffect(() => {
-    // Inject Botpress script dynamically
-    const script = document.createElement("script");
-    script.src = "https://cdn.botpress.cloud/webchat/v2.4/inject.js";
-    script.async = true;
-    script.onload = () => {
-      window.botpressWebChat.init({
-        configUrl:
-          "https://files.bpcontent.cloud/2025/04/23/17/20250423172151-6PCWRVYD.js",
-        selector: "#webchat",
-      });
-    };
-    document.body.appendChild(script);
-
-    // Poll quote sync API
     const poll = setInterval(async () => {
       const res = await fetch("/api/quote-sync");
       const data = await res.json();
-      if (
-        data?.product &&
-        !quoteItems.find((i) => i.name === data.product.name)
-      ) {
+      if (data?.product && !quoteItems.find((i) => i.name === data.product.name)) {
         setQuoteItems((prev) => [...prev, data.product]);
       }
     }, 5000);
@@ -37,7 +21,25 @@ export default function Home() {
       {/* Chat Area */}
       <div className="w-1/2 p-6 border-r border-gray-200">
         <h2 className="text-xl font-semibold mb-4">Audico Chat</h2>
-        <div id="webchat" className="h-[90vh] w-full" />
+        <div
+          className="h-[90vh] w-full"
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe
+                style="height: 100%; width: 100%; border: none;"
+                srcdoc='
+                  <!doctype html>
+                  <html lang="en">
+                    <head></head>
+                    <body>
+                      <script src="https://cdn.botpress.cloud/webchat/v2.4/inject.js"></script>
+                      <script src="https://files.bpcontent.cloud/2025/04/23/17/20250423172151-6PCWRVYD.js"></script>
+                    </body>
+                  </html>'
+              ></iframe>
+            `,
+          }}
+        ></div>
       </div>
 
       {/* Quote Panel */}
